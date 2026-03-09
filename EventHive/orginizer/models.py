e10911d75345aaccdd8cd0f307f2e4d8f302cde5
valentinemukari46-prog <valentinemukari46@gmail.com>
2026-03-09 10:53:22 +0300

    model.py update
    created model.py in the app venue

--- EventHive/orginizer/models.py
+++ EventHive/orginizer/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Orginizer(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=50)
+    company = models.CharField(max_length=50) 
+
+    def __str__(self):
+        return self.orginizer_name
